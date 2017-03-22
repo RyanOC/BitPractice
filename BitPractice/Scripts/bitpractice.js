e23aa36d192963d7a0b253cff2c0074b1ef152e8
RyanOC <rconnolly.rdk@outlook.com>
2017-03-21 22:49:30 -0400

    Added multiple bit options with onclick event listeners to load its start and end time.

--- BitPractice/Scripts/bitpractice.js
+++ BitPractice/Scripts/bitpractice.js
@@ -4,6 +4,8 @@ tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player, startTime, endTime, timeInterval, vid;
+var bitStart = $("#startTime").val();
+var bitEnd = $("#endTime").val();
 
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('player', {
@@ -25,9 +27,9 @@ function SetVideoId() {
     clearInterval(timeInterval);
 
     vid = $("#videoid").val();
-    startTime = convertTime($("#startTime").val());
-    endTime = convertTime($("#endTime").val());
- 
+    startTime = convertTime(bitStart);
+    endTime = convertTime(bitEnd);
+
     timeInterval = setInterval(function () {
         GetTime();
     }, 0);
@@ -56,11 +58,11 @@ function GetTime() {
     }
 
     var current = player.getCurrentTime();
-    current = current.toFixed(2);    
+    current = current.toFixed(2);
     $('#currentSeconds').html(current);
     var minutes = Math.floor(current / 60);
     var seconds = current % 60;
-    seconds = parseFloat(seconds).toFixed(2);     
+    seconds = parseFloat(seconds).toFixed(2);
     $('#currentTime').html(minutes + ":" + seconds);
 }
 
@@ -72,19 +74,81 @@ function convertTime(input) {
 }
 
 function reset() {
-
     startTime = convertTime($("#startTime").val());
     endTime = convertTime($("#endTime").val());
     player.seekTo(startTime); //parseFloat($("#seekto").val()));
-
 }
 
 $(window).load(function () {
     //jQuery('#seek').click(function(){
-    startTime = convertTime($("#startTime").val());
-    endTime = convertTime($("#endTime").val());
+    startTime = convertTime(bitStart);
+    endTime = convertTime(bitEnd);
     player.seekTo(startTime); //parseFloat($("#seekto").val()));
     //player.setPlaybackRate(0.5); // 0.25, 0.5, 1, 1.5, and 2
     //return false;
+    //});
+});
+
+
+
+
+$('.bitSelector').click(
+    function (e) {
+        e.preventDefault;
+        $('.highlight').removeClass('highlight');
+        $(this).addClass('highlight');
+        var id = $(this).attr('id');
+        var bitSpanDiv = $(this).html();
+
+        $('input', bitSpanDiv).each(function (index) {
+
+            console.log($(this).val());
+
+            if (index % 2 == 0) {
+                //bitStart = $(this).val().toString();
+                console.log($(this).val());
+                startTime = convertTime($(this).val());
+            }
+            else {
+                //bitEnd = $(this).val().toString();   
+                console.log($(this).val());
+                endTime = convertTime($(this).val());
+            }
+        })
+
+        player.seekTo(startTime);
+    });
+
+
+$(document).ready(function () {
+
+    //$(".add-more").click(function () {
+    //    //var html = $(".copy").html();
+    //    var html = $(".copy").clone();
+    //    //var result = $('input', html);
+
+    //    $('input', html).each(function () {
+
+    //        console.log($(this).val());
+
+    //        $(this).val('0:00');
+
+    //        if (bitCount % 2 == 0) {
+    //            $(this).attr('id', 'end' + bitCount);
+    //        }
+    //        else {
+    //            $(this).attr('id', 'start' + bitCount);
+    //        }
+    //        //arrNumber.push($(this).val());
+    //        bitCount += 1;
+    //    })
+    //    // .appendTo
+    //    //$(".after-add-more").after(html);
+
+    //    html.appendTo(".after-add-more");
+    //});
+
+    //$("body").on("click", ".remove", function () {
+    //    $(this).parents(".control-group").remove();
     //});
 });
